f189a6c04329888a06de00b0e33d96728c494726
J <jcnguyen1994@gmail.com>
2018-11-28 23:43:27 -0500

    Working Graph
    Working Graph

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ function start() {
 
     var width = 1000;
     var height = 1500;
-
-    
-
+    var r = d3.scaleLinear().domain([0, 10]).range([0, 7]);
+    var c = d3.scaleOrdinal(d3.schemeCategory10).domain(["Australia", "Bahamas", "Belgium", "Brazil", "Bulgaria", "Cambodia", "Canada", "Chile", "China", 
+    "Czech Republic", "Denmark", "Egypt", "Finland", "France", "Georgia", "Germany", "Greece", "Hong Kong",
+    "Hungary", "Iceland", "India", "Indonesia", "Iran", "Ireland", 
+    "Israel", "Italy", "Japan", "Kenya", "Kyrgyzstan", "Mexico", "New Zealand",
+    "Nigeeria", "Norway", "Official Site", "Pakistan", "Panama", "Poland", 
+    "Romania", "Russia", "Slovenia", "South Africa", "Spain", "Sweden", "Switzerland", 
+    "Taiwan", "Thailand", "UK", "USA"]);
     var svg = d3.select(graph)
         .append('svg')
         .attr('width', width)
@@ -21,7 +26,7 @@ function start() {
             .range([0, width]);
         var x = d3.scaleLinear()
             .domain([0, 10])
-            .range([height, 0]);
+            .range([height, 0]);    
 
 
 
@@ -31,8 +36,35 @@ function start() {
         //to make budget a number
         d.budget = +d.budget;
         return d;
+        
     }, (error, data) => {
-
+        svg.selectAll("circle").data(data).enter().append("circle")
+        .attr("cx", function(d) {
+          return x(0);
+        })
+        .attr("cy", function(d) {
+          return y(0);
+        })
+        .attr("r", function(d) {
+          return r(0);
+        })
+        .style("fill", function(d) {
+            return c(d.country);
+          })
+        .append("title")
+          .text(function (d) {
+            return d.movie_title;
+          })
+        svg.selectAll("circle").transition().duration(1000)
+          .attr("cx", function(d) {
+            return x(+d.imdb_score);
+          })
+          .attr("cy", function(d) {
+            return y(+d.budget);
+          })
+          .attr("r", function(d) {
+            return r(5);
+          })
         var imdbScore = d3.nest().key(d => {
                 return d.imdb_score;
             }).sortKeys(d3.ascending)
@@ -40,7 +72,8 @@ function start() {
                 return v.map(s => {
                     return {
                         title: s.movie_title,
-                        budget: s.budget
+                        budget: s.budget,
+                        country: s.country
                     }
                 });
             })
@@ -48,8 +81,8 @@ function start() {
 
         console.log(imdbScore);
     });
+    
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
     var formatNumber = d3.format(".1f");
 
     var x = d3.scaleLinear()
@@ -57,7 +90,7 @@ function start() {
         .range([0, width/1.19]);
 
     var y = d3.scaleLinear()
-        .domain([0, 650000000])
+        .domain([0, 300000000])
         .range([height, 0]);
 
     var xAxis = d3.axisBottom(x)
